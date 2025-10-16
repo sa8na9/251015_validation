d66053496b6559955ac509128b33316dee7a68af
sana <wfwfsan@gmail.com>
2025-10-16 18:46:15 +0900

    new commit

--- script.js
+++ script.js
@@ -1,5 +1,119 @@
-const button_next = document.getElementById('button_next');
+// 入力値の正当性
+const checkList = {'sei_kanji': false, 'mei_kanji': false, 'shi_kana': false, 'mei_kana': false, 'gender': false, 'age': false};
 
-button_next.addEventListener('click', () => {
-    alert("ボタンクリック");
+// エラー文表示関数                 id・エラー内容ごとのメッセージを表示したいので、後述のメイン関数で引数に積んでもらう
+function displayError(subjectId, message) {
+    const errorSubject = document.getElementById(`error_${subjectId}`);
+    // 対象にエラーが発生していたら
+    if (errorSubject) {
+        errorSubject.textContent = message; // idごとのメッセージを表示
+    }
+}
+
+// エラー文削除関数
+function deleteError(subjectId) {
+    const correctSubject = document.getElementById(`error_${subjectId}`)
+    // 対象が正常に入力されていたら
+    if (correctSubject) {
+        correctSubject.textContent = ''; // エラー文削除
+    }
+}
+
+// 入力値チェック関数
+function inputCheck(user_input) {
+    let error_message = '';
+    
+    // 必須入力チェック
+    if (user_input.value === '') {
+        displayError(user_input.id, '入力は必須です');
+        checkList[user_input.id] = false;
+        console.log(checkList[user_input.id]);
+        switchingButton();
+        return; // 入力されていなければ以下のチェックは不要
+    }
+
+    // 入力値チェック
+    switch (user_input.id) {
+        // <姓名>　数字・記号は弾く
+        case 'sei_kanji':
+        case 'mei_kanji':
+            if (/[^a-zA-Zー\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF]/.test(user_input.value)) {
+                checkList[user_input.id] = false;
+                error_message = '数字、記号、空白は使えません'
+            } else {
+                checkList[user_input.id] = true;
+            }
+            break;
+
+        // <氏名>　カナ以外を弾く
+        case 'shi_kana':
+        case 'mei_kana':
+            if (/[^ァ-ヶー]/.test(user_input.value)) {
+                checkList[user_input.id] = false;
+                error_message = 'カタカナで入力してください'
+            } else {
+                checkList[user_input.id] = true;
+            }
+            break;
+
+        case 'age':
+            if (/[^0-9]/.test(user_input.value)) {
+                checkList[user_input.id] = false;
+                error_message = '半角数字で入力してください';
+            } else {
+                checkList[user_input.id] = true;
+            }
+    }
+
+    if (error_message) {
+        displayError(user_input.id, error_message);
+    } else {
+        deleteError(user_input.id);
+    }
+    console.log(checkList[user_input.id]);
+    isCheckedGender();
+    switchingButton();
+};
+
+// 性別のラジオボタンがなにかしら選択されているか判別する関数
+function isCheckedGender() {
+    const radio = document.querySelector('input[name="radio_gender"]:checked');
+        if (radio) {
+            checkList['gender'] = true;
+            return;
+        } else {
+            checkList['gender'] = false;
+        }
+}
+
+// ラジオボタンの監視
+document.querySelectorAll('input[name="radio_gender"]').forEach(radio => {
+    radio.addEventListener('change', switchingButton);
+});
+
+// ボタンの活性状況を操作する関数
+function switchingButton() {
+    isCheckedGender();
+    for (const id in checkList) {
+        if (checkList[id] == false) {
+            document.querySelector('.btn_next').disabled = true;
+            return;
+        }
+    }
+    document.querySelector('.btn_next').disabled = false;
+}
+
+// enterキーの無効化
+if (document.getElementById('form_submit')) {
+    document.getElementById('form_submit').addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    })
+}
+
+// 次へボタン押下
+document.form_submit.btn_next.addEventListener('click', () => {
+    alert('ボタン押下')
+    // document.form_submit.submit();
 });
